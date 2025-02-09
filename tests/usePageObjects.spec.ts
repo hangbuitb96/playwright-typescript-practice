a41dd9eb6cd2cdab2e92093b7a76969a74cb1475
Hang Bui <hangthu.bui@outlook.com>
2025-02-09 22:57:01 +0700

    npm scripts and test data generator

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
+import {faker} from '@faker-js/faker'
 
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
@@ -16,9 +17,14 @@ test('navigate to form page', async({page}) =>{
 
 test('parametrized methods', async({page}) => {
     const pm = new PageManager(page)
+
+    //generate random test data
+    const randomFullName = faker.person.fullName() //generate random fullnames
+    const randomEmail = `${randomFullName.replace(/\s+/g, '')}${faker.number.int(1000)}@test.com` // randomFullName.replace(/\s+/g, '') to remove all whitespaces
+
     await pm.navigateTo().formLayoutPage()
     await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialAndSelectOption("test@test.com", "abcabc", "Option 1")
-    await pm.onFormLayoutsPage().submitInlineFormWithCredentialAndSelectOption("Hang", 'hang@test.com', false)
+    await pm.onFormLayoutsPage().submitInlineFormWithCredentialAndSelectOption(randomFullName, randomEmail, false)
 
     await pm.navigateTo().datePickerPage()
     await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(7)
